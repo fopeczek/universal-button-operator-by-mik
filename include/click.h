f371fbc02e8221528c557d089c498592f0df462c
Mikołaj Krawczuk <mikolaj@statystyka.net>
2021-04-17 12:25:16 +0200

    Sea of thiefs

--- include/click.h
+++ include/click.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "function_objects.h"
+#include "CapacitiveSensor.h"
 enum class click_state {pulled, grace_period_pulled, pressed, grace_period_pressed};
 
 void nofun();
@@ -15,13 +16,13 @@ public:
 	void setupTurnOnHandler(FunctionObject<void(void)> event_on);
 	void setupTurnOffHandler(FunctionObject<void(void)> event_off);
 
+	int m_pin;
 private:
 	bool m_use_click;
 	bool m_use_hold;
 	bool m_use_on;
 	bool m_use_off;
 
-	int m_pin;
 	bool m_analog_pin=false;
 
 	FunctionObject<void(void)> m_event_click;
@@ -38,7 +39,11 @@ private:
 
 class GuzikTrio {
 public:	
-
+	GuzikTrio(int type) {
+		if (type == 1 or type == 2){//1 normal, 2 second is first and third off
+			m_type = type;
+		}
+	}
 	void setupFirstUsingDigitalPin(int pin);
 	void setupFirstUsingAnalogPin(int pin);
 
@@ -59,4 +64,32 @@ private:
 	Guzik st;
 	Guzik nd;
 	Guzik rd;
+	int m_type;
+	void is_it_second();
+};
+
+class GuzikCap {
+public:
+	GuzikCap(long click_value = 1000) : m_pin(-1), m_use_hold(false), m_pressed(false), m_event_click(nofun), m_event_hold(nofun), m_CapButton(CapacitiveSensor(-1,-1)) { 
+		m_click_value = click_value;
+	}
+	void setupCapacitivePin(int pin);
+
+	void setupCapacitiveClick(FunctionObject<void(void)> event_click);
+	void setupCapacitiveHold(FunctionObject<void(void)> event_hold);
+
+	void update();
+private:
+	int m_pin;
+	
+	bool m_use_hold = false;
+	bool m_pressed = false;
+
+	long m_click_value = 1000;
+	long m_pressed_time;
+
+	FunctionObject<void(void)> m_event_click;
+	FunctionObject<void(void)> m_event_hold;
+
+	CapacitiveSensor m_CapButton;
 };
--- src/click.cpp
+++ src/click.cpp
@@ -39,7 +39,7 @@ void Guzik::update(bool debug) {
 	} else if (m_state==click_state::grace_period_pressed){
 		if (not read && millis() - m_fast_t >= GRACE_PERIOD) {//jeżeli minie czas grace period
 			m_state=click_state::pulled;//                       to znaczy że puszczono
-			if (millis() - m_czas_n >= 1000){
+			if (millis() - m_czas_n >= CZAS_TRZYMANIA){
 				if (m_use_hold){
 					m_event_hold();
 				} else if (m_use_click) {
@@ -101,6 +101,9 @@ void GuzikTrio::setupFirstUsingAnalogPin(int pin) {
 
 void GuzikTrio::setupSecondUsingAnalogPin(int pin) {
 	nd.setupUsingAnalogPin(pin);
+	if (m_type == 2){
+		Serial.print("you must use type 1 to declare second pin!");
+	}
 }
 
 void GuzikTrio::setupThirdUsingAnalogPin(int pin) {
@@ -113,27 +116,95 @@ void GuzikTrio::setupFirstUsingDigitalPin(int pin) {
 
 void GuzikTrio::setupSecondUsingDigitalPin(int pin) {
 	nd.setupUsingDigitalPin(pin);
+	if (m_type == 2){
+		Serial.print("you must use type 1 to declare second pin!");
+	}
 }
 
 void GuzikTrio::setupThirdUsingDigitalPin(int pin) {
 	rd.setupUsingDigitalPin(pin);
 }
 
+FunctionObject<void(void)> m_event_nd;
+
+void GuzikTrio::is_it_second(){
+	if (digitalRead(st.m_pin) == 0 and digitalRead(rd.m_pin) == 0){
+		m_event_nd();
+	}
+}
+
+//TODO: Make sure GuzikTrio cannot be automatic
 
 void GuzikTrio::setupTurnFirstHandler(FunctionObject<void(void)> event_st){
 	st.setupTurnOnHandler(event_st);
+	if (m_type == 2){
+		st.setupTurnOffHandler([this](){this->is_it_second();});
+	}
 }
 
 void GuzikTrio::setupTurnSecondHandler(FunctionObject<void(void)> event_nd){
-	nd.setupTurnOnHandler(event_nd);
+	if (m_type == 1){
+		nd.setupTurnOnHandler(event_nd);
+	} else if (m_type == 2){
+		m_event_nd = event_nd;
+	}
 }
 
 void GuzikTrio::setupTurnThirdHandler(FunctionObject<void(void)> event_rd){
 	rd.setupTurnOnHandler(event_rd);
+	if (m_type == 2){
+		rd.setupTurnOffHandler([this](){this->is_it_second();});
+	}
 }
 
 void GuzikTrio::update(bool debug){
 	st.update(debug); 
 	nd.update(debug);
 	rd.update(debug);
+}
+
+void GuzikCap::setupCapacitivePin(int pin) {
+	m_pin = pin;
+	if (m_pin == 2) {
+		m_CapButton = CapacitiveSensor(4,2);
+	}
+	if (m_pin == 6) {
+		m_CapButton = CapacitiveSensor(4,6);
+	}
+	if (m_pin == 8) {
+		m_CapButton = CapacitiveSensor(4,8);
+	}
+}
+
+void GuzikCap::setupCapacitiveClick(FunctionObject<void(void)> event_click){ 
+	m_event_click = event_click;
+}
+
+void GuzikCap::setupCapacitiveHold(FunctionObject<void(void)> event_hold){
+	m_event_hold = event_hold;
+	m_use_hold = true;
+}
+
+void GuzikCap::update(){
+	long read = m_CapButton.capacitiveSensor(30);
+	if (read >= m_click_value){
+		if (m_pressed == false){
+			m_pressed = true;
+			m_pressed_time = millis();
+		}
+	}
+	if (read <= m_click_value){
+		if (m_pressed == true){
+			m_pressed = false;
+			if (millis() - m_pressed_time >= CZAS_TRZYMANIA){
+				if (m_use_hold){
+					m_event_hold();
+				} else {
+					m_event_click();
+				}
+			} else {
+				m_event_click();
+			}
+		}
+	}
 }
